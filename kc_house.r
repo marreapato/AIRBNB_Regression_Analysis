1581639307f11e79c2abef702c1c310cebd87b5f
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-09-21 20:53:27 -0300

    Update kc_house.r

--- kc_house.r
+++ kc_house.r
@@ -8,3 +8,11 @@ mean(dados$bathrooms)
 
 hist(dados$bathrooms)
 hist(dados$bedrooms)
+hist(dados$sqft_basement)
+hist(dados$sqft_living)
+hist(dados$sqft_above)
+hist(dados$grade)
+
+ggplot(dados,aes(y=dados$bedrooms,x=as.factor(dados$waterfront)))+geom_boxplot()
+#analisar outliers
+
