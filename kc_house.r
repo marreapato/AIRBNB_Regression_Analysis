0f341c9f88478de353fa50646f803722ee3b6e9d
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-09-22 10:45:12 -0300

    Update kc_house.r

--- kc_house.r
+++ kc_house.r
@@ -15,5 +15,8 @@ hist(dados$sqft_living)
 hist(dados$sqft_above)
 hist(dados$grade)
 
+ggplot(dados,aes(x=dados$bedrooms))+geom_histogram()+theme_economist_white()+labs(title="Vista para lagos ou rios")+scale_fill_continuous_tableau()+theme(legend.position = "none")+stat_bin(bins = 20) 
+
 ggplot(dados,aes(y=dados$bedrooms,x=as.factor(dados$waterfront),fill=as.factor(dados$waterfront)))+geom_boxplot()+theme_fivethirtyeight()+labs(title="Vista para lagos ou rios")+scale_x_discrete(labels=c("Não Possui","Possui"))+scale_fill_fivethirtyeight()+theme(legend.position = "none") 
 #analisar outliers
+
