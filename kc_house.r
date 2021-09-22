7944157dfe766f250eb66a048061a7c70c11bfdd
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-09-22 10:37:22 -0300

    Update kc_house.r

--- kc_house.r
+++ kc_house.r
@@ -1,5 +1,7 @@
 #install.packages("tidyverse")
+#install.packages("ggthemes")
 library(tidyverse)
+library(ggthemes)
 dados <- read.csv("kc_house_data.csv")
 
 summary(dados)
@@ -13,6 +15,6 @@ hist(dados$sqft_living)
 hist(dados$sqft_above)
 hist(dados$grade)
 
-ggplot(dados,aes(y=dados$bedrooms,x=as.factor(dados$waterfront)))+geom_boxplot()
+ggplot(dados,aes(y=dados$bedrooms,x=as.factor(dados$waterfront),fill=as.factor(dados$waterfront)))+geom_boxplot()+theme_fivethirtyeight()+labs(title="Vista para lagos ou rios")+scale_x_discrete(labels=c("Não Possui","Possui"))+scale_fill_fivethirtyeight()+theme(legend.position = "none") 
 #analisar outliers
 
